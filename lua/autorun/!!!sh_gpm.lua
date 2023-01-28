90c2812b1846220739602d33525b3fe5c5cb432e
PrikolMen <olegis019@gmail.com>
2023-01-28 19:02:37 +0400

    Nice prints

--- lua/autorun/!!!sh_gpm.lua
+++ lua/autorun/!!!sh_gpm.lua
@@ -2,11 +2,10 @@ local AddCSLuaFile = AddCSLuaFile
 local include = include
 local SERVER = SERVER
 
-local startTime = SysTime()
-
 gpm = gpm or {}
 gpm._VERSION = 000001
 
+-- Include function
 local workFolder = "gpm/"
 local function includeShared( fileName )
     if (SERVER) then
@@ -16,21 +15,28 @@ local function includeShared( fileName )
     include( workFolder .. fileName )
 end
 
+-- Loading start time
+local startTime = SysTime()
+
+-- Global & Debug functions
 includeShared( "globals.lua" )
 includeShared( "debug.lua" )
 
+-- Colors & Logger modules
 includeShared( "colors.lua" )
 includeShared( "logger.lua" )
 
+-- Global GPM Logger Creating
 local color = HEXToColor( "#AEC5EB" )
 gpm.colors.Set( "gpm", color )
 gpm.Logger = gpm.logger.Create( "Glua Package Manager", color )
 
+-- Environment & Zip modules
 includeShared( "environment.lua" )
 includeShared( "unzip.lua" )
 
+-- Importer module
 includeShared( "importer.lua" )
 
+-- Finish log
 gpm.Logger:Info( "Time taken to start-up: %.4f sec.", SysTime() - startTime )
-
--- PrintTable( gpm )
