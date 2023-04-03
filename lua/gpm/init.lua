2b7dfb83c7c2a425def53aa1686e66b3c1c10356
PrikolMen <olegis019@gmail.com>
2023-04-03 21:22:17 +0400

    Update init.lua

--- lua/gpm/init.lua
+++ lua/gpm/init.lua
@@ -1,10 +1,12 @@
+-- Libraries
+local file = file
+
 -- Functions
 local AddCSLuaFile = AddCSLuaFile
 local include = include
-local file = file
 
-gpm = gpm or {}
-gpm._VERSION = 000001
+module( "gpm" )
+_VERSION = 000001
 
 -- Include function
 function includeShared( fileName )
@@ -30,16 +32,17 @@ local startTime = SysTime()
 -- Utils
 includeShared "utils"
 
--- Promises
-includeShared "promise"
-
 -- Colors & Logger modules
 includeShared "colors"
 includeShared "logger"
 
 -- Global GPM Logger Creating
-gpm.colors.Set( "gpm", Color(174, 197, 235) )
-gpm.Logger = gpm.logger.Create( "Glua Package Manager (" .. gpm._VERSION  .. ")", color )
+colors.Set( "gpm", Color(174, 197, 235) )
+Logger = logger.Create( "GPM (" .. utils.Version( _VERSION ) .. ")", color )
+
+-- Promises
+includeShared "promise"
+Logger:Info( "Promise the library version %s is initialized.", utils.Version( promise._VERSION_NUM ) )
 
 -- Environment & Zip modules
 includeShared "environment"
@@ -48,11 +51,11 @@ includeShared "unzip"
 includeShared "packages"
 
 -- Sources
-gpm.sources = gpm.sources or {}
+sources = sources or {}
 includeShared "sources"
 
 -- Importer module
 includeShared "importer"
 
 -- Finish log
-gpm.Logger:Info( "Time taken to start-up: %.4f sec.", SysTime() - startTime )
+Logger:Info( "Time taken to start-up: %.4f sec.", SysTime() - startTime )
