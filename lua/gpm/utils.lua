854e305e1bfedba40df067c1ccb8da8ca34b558f
PrikolMen <olegis019@gmail.com>
2023-04-03 21:12:07 +0400

    Some `globals` moved to `utils`

--- lua/gpm/utils.lua
+++ lua/gpm/utils.lua
@@ -1,13 +1,59 @@
-local debug_getinfo = debug.getinfo
-local module = module
+-- Libraries
 local string = string
 local table = table
 
+-- Functions
+local debug_getinfo = debug.getinfo
+local tonumber = tonumber
+local module = module
+local error = error
+local pairs = pairs
+local type = type
+
+-- Checks if argument have valid type
+function ArgAssert( value, argNum, expected, errorlevel )
+    local valueType = type( value )
+    if valueType == expected then return end
+
+    local dinfo = debug_getinfo( 2, "n" )
+    error( string.format( "bad argument #%d to \'%s\' (%s expected, got %s)", argNum, dinfo and dinfo.name or "func", expected, valueType ), errorlevel or 3 )
+end
+
+--
+do
+
+    local TYPE_FILE = TYPE_FILE
+    local TypeID = TypeID
+
+    function isFile( any )
+        return TypeID( any ) == TYPE_FILE
+    end
+
+end
+
+-- Make JIT happy
+function debug.fempty()
+end
+
 module( "gpm.utils" )
+
+function LowerTableKeys( tbl )
+    for key, value in pairs( tbl ) do
+        if type( value ) == "table" then value = LowerTableKeys( value ) end
+        if type( key ) ~= "string" then continue end
+        tbl[ key ] = nil; tbl[ string.lower( key ) ] = value
+    end
+end
+
+function Version( number )
+    if not number then return "invalid version" end
+    local version = string.format( "%06d", number )
+    return string.format( "%d.%d.%d", tonumber( string.sub( version, 0, 2 ) ), tonumber( string.sub( version, 3, 4 ) ), tonumber( string.sub( version, 5 ) ) )
+end
 
 function GetCurrentFile()
     for i = 2, 6 do
-        local info = debug_getinfo(i, "S")
+        local info = debug_getinfo( i, "S" )
         if not info then break end
         if info.what == "main" then return info.short_src end
     end
@@ -25,7 +71,7 @@ end
 
 -- File path join
 function Join( filePath, ... )
-    return path.Fix( table.concat( { filePath, ... }, "/" ) )
+    return Fix( table.concat( { filePath, ... }, "/" ) )
 end
 
 -- File path localization
