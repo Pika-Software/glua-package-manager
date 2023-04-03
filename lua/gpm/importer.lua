72d81b87b9e79c5d375af908ffc88cefb45199af
PrikolMen <olegis019@gmail.com>
2023-04-03 21:13:24 +0400

    Update importer.lua

--- lua/gpm/importer.lua
+++ lua/gpm/importer.lua
@@ -1,10 +1,14 @@
+-- Libraries
+local packages = gpm.packages
+local promise = gpm.promise
+local string = string
 
+-- Functions
 local IterateZipFiles = gpm.unzip.IterateZipFiles
 local CompileString = CompileString
 local ArgAssert = ArgAssert
-local promise = gpm.promise
 
-module("gpm.importer", package.seeall)
+module( "gpm.importer", package.seeall )
 
 local LUA_REALM = SERVER and "lsv" or "lcl"
 
@@ -16,105 +20,88 @@ function CompileLuaFuncs( iter )
         local fileName, data
         repeat
             fileName, data = iter()
-            if fileName and fileName:EndsWith(".lua") then
-                return fileName, CompileString(data or "", fileName)
+            if fileName and string.EndsWith( fileName, ".lua" ) then
+                return fileName, CompileString( data or "", fileName )
             end
         until fileName == nil
     end
 end
 
-function ParsePackageInfo(tbl)
-    if not istable(tbl) then return end
-
-    local info = {}
-    info.name = isstring(tbl.name) and tbl.name or "no name"
-    info.version = isstring(tbl.version) and tbl.version or "0.0.1"
-    info.main = isstring(tbl.main) and tbl.main or nil
-
-    return info
-end
-
-function LoadPackageInfoFromFunc(func)
-    if isstring(func) then func = CompileString(func) end
-    if not isfunction(func) then return end
-
-    setfenv(func, {})
-    local ok, info = xpcall(func, ErrorNoHaltWithStack)
-    return ok and ParsePackageInfo(info)
-end
-
 function ImportZIP( pathToArchive )
     local f
-    if pathToArchive:StartWith("data/") then
-        f = file.Open(pathToArchive:sub(6), "rb", "DATA")
+    if string.StartWith( pathToArchive, "data/" ) then
+        f = file.Open( string.sub( pathToArchive, 6 ), "rb", "DATA")
     end
-    if not f then return ErrorNoHaltWithStack("file not found") end
+
+    if not f then return ErrorNoHaltWithStack( "file not found" ) end
 
     local packageInfo
     local files = {}
-    for fileName, func in CompileLuaFuncs( IterateZipFiles(f) ) do
-        if fileName:StartWith("lua/") then fileName = fileName:sub(5) end
+    for fileName, func in CompileLuaFuncs( IterateZipFiles( f ) ) do
+        if string.StartWith( fileName, "lua/" ) then fileName = string.sub( fileName, 5 ) end
 
-        if fileName == "package.lua" then
-            packageInfo = LoadPackageInfoFromFunc(func)
+        if ( fileName == "package.lua" ) then
+            packageInfo = packages.GetMetaData( func )
         else
-            files[fileName] = func
+            files[ fileName ] = func
         end
     end
 
     f:Close()
 
-    if not packageInfo then return ErrorNoHaltWithStack("package.lua not found") end
-    if not packageInfo.main or not files[packageInfo.main] then return ErrorNoHaltWithStack("no main file provided") end
+    if not packageInfo then return ErrorNoHaltWithStack( "package.lua not found" ) end
+    if not packageInfo.main or not files[ packageInfo.main ] then
+        return ErrorNoHaltWithStack( "no main file provided" )
+    end
 
     packageInfo.ImportedFrom = "ZIP"
     packageInfo.ImportedExtra = nil
 
     local main = files[ packageInfo.main ]
-    return gpm.package.InitializePackage( packageInfo, main, files )
+    return gpm.packages.InitializePackage( packageInfo, main, files )
 end
 
 LocalFilesFinderMeta = LocalFilesFinderMeta or {}
-LocalFilesFinderMeta.__index = function(self, fileName)
-    if isstring(fileName) and fileName:EndsWith(".lua") and file.Exists(fileName, LUA_REALM) then
-        self[fileName] = CompileFile(fileName) -- Caching result
-        return self[fileName]
+LocalFilesFinderMeta.__index = function( self, fileName )
+    if isstring( fileName ) and string.EndsWith( fileName, ".lua" ) and file.Exists( fileName, LUA_REALM ) then
+        self[ fileName ] = CompileFile( fileName ) -- Caching result
+        return self[ fileName ]
     end
 end
 
-function ImportLocal(fileName)
-    local packageFileName = fileName:EndsWith("package.lua") and fileName
+function ImportLocal( fileName )
+    local packageFileName = string.EndsWith( fileName, "package.lua" ) and fileName
     if not packageFileName then packageFileName = fileName .. "/package.lua" end
-    if not file.Exists(packageFileName, LUA_REALM) then return ErrorNoHaltWithStack("file not found") end
+    if not file.Exists( packageFileName, LUA_REALM ) then return ErrorNoHaltWithStack( "file not found" ) end
 
-    local packageInfo = LoadPackageInfoFromFunc( CompileFile(packageFileName) )
-    if not packageInfo then return ErrorNoHaltWithStack("invalid package.lua") end
+    local packageInfo = packages.GetMetaData( CompileFile( packageFileName ) )
+    if not packageInfo then return ErrorNoHaltWithStack( "invalid package.lua" ) end
 
-    local mainFile = packageInfo.main and file.Exists(packageInfo.main, LUA_REALM) and CompileFile(packageInfo.main, "package.lua")
-    if not mainFile then return ErrorNoHaltWithStack("failed to include main file") end
+    local mainFile = packageInfo.main and file.Exists( packageInfo.main, LUA_REALM ) and CompileFile( packageInfo.main, "package.lua" )
+    if not mainFile then return ErrorNoHaltWithStack( "failed to include main file" ) end
 
-    AddCSLuaFile(packageFileName)
-    AddCSLuaFile(packageInfo.main)
+    AddCSLuaFile( packageFileName )
+    AddCSLuaFile( packageInfo.main )
 
-    local files = setmetatable({}, LocalFilesFinderMeta)
-    files[packageInfo.main] = mainFile
+    local files = setmetatable( {}, LocalFilesFinderMeta )
+    files[ packageInfo.main ] = mainFile
 
     packageInfo.ImportedFrom = "Local"
     packageInfo.ImportedExtra = nil
 
-    return gpm.package.InitializePackage( packageInfo, mainFile, files )
+    return gpm.packages.InitializePackage( packageInfo, mainFile, files )
 end
 
 AsyncImport = promise.Async(function( fileName)
-    if fileName:StartWith("data/") then
-        if fileName:EndsWith(".zip.dat") then
-            return ImportZIP(fileName)
-        elseif filename:EndsWith(".gma.dat") then
-            print("ToDo") -- ToDo
+    if string.StartWith( fileName, "data/" ) then
+        if string.EndsWith( fileName, ".zip.dat" ) then
+            return ImportZIP( fileName )
+        elseif string.EndsWith( filename, ".gma.dat" ) then
+            print( "ToDo" ) -- ToDo
         end
-    elseif fileName:StartWith("lua/") then
-        fileName = fileName:sub(5)
-        return gpm.loaders.lua.Import(fileName)
+    elseif string.StartWith( fileName, "lua/" ) then
+        fileName = string.sub( fileName, 5 )
+        return gpm.sources.lua.Import( fileName )
     end
 end )
 
