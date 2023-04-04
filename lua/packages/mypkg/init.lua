42e0e681b3862b7d2f8cc09437be1a1ab844862f
PrikolMen <olegis019@gmail.com>
2023-04-04 13:38:46 +0400

    tests

--- lua/packages/mypkg/init.lua
+++ lua/packages/mypkg/init.lua
@@ -1,6 +1,18 @@
-MYMODULE = {}
+MYMODULE = MYMODULE or {}
 
-AddCSLuaFile("helloworld.lua")
+-- print( import "https://raw.githubusercontent.com/PrikolMen/gpm_web_package_test/main/i_love_github.json" )
+-- local tbl = import "https://raw.githubusercontent.com/PrikolMen/gpm_web_package_test/main/test.lua"
+
+-- PrintTable( tbl )
+
+-- print( import( "packages/mypkg2" ).Test() )
+
+if SERVER then
+    AddCSLuaFile("helloworld.lua")
+end
+
 include("helloworld.lua")
+
+MYMODULE.HelloWorld()
 
 return MYMODULE
