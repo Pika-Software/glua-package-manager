bff50c2d03e15fbef97a10a90e82d970dec143d2
PrikolMen <olegis019@gmail.com>
2023-04-06 07:57:23 +0400

    Update environment.lua

--- lua/gpm/environment.lua
+++ lua/gpm/environment.lua
@@ -1,74 +1,112 @@
+-- Libraries
+local table = table
+
 -- Variables
-local table_SetValue = table.SetValue
-local setmetatable = setmetatable
 local debug_fcopy = debug.fcopy
 local ArgAssert = ArgAssert
 local setfenv = setfenv
-local pairs = pairs
 local type = type
-local _G = _G
 
-module( "gpm.environment" )
+module( "gpm.environment", package.seeall )
 
 function SetValue( env, path, func, makeCopy )
     ArgAssert( func, 1, "function" )
-    return table_SetValue( env, path, setfenv( makeCopy and debug_fcopy( func ) or func, env ) )
+    return table.SetValue( env, path, setfenv( makeCopy and debug_fcopy( func ) or func, env ) )
 end
 
 do
 
-    local metaCache = {}
+    local getmetatable = getmetatable
+    local setmetatable = setmetatable
+    local rawget = rawget
+    local rawset = rawset
+    local ipairs = ipairs
+
+    local meta = {}
+
+    function meta:__index( key )
+        local indexes = rawget( self, "__indexes" )
+        if indexes ~= nil then
+            for _, index in ipairs( indexes ) do
+                local value = index[ key ]
+                if value == nil then continue end
+
+                return value
+            end
+        end
+
+        -- return rawget( self, key )
+    end
 
     function LinkTables( a, b )
         ArgAssert( a, 1, "table" )
         ArgAssert( b, 2, "table" )
 
-        local meta = metaCache[ b ]
-        if meta == nil then
-            meta = {__index = b}; metaCache[ b ] = meta
+        local aMeta = getmetatable( a )
+        if aMeta ~= meta then
+            setmetatable( a, meta )
+
+            local indexes = rawget( a, "__indexes" )
+            if type( indexes ) ~= "table" then
+                indexes = {}; rawset( a, "__indexes", indexes )
+            end
+
+            table.RemoveByValue( indexes, b )
+            table.insert( indexes, 1, b )
         end
 
-        setmetatable( a, meta )
         return a
     end
 
 end
 
-function Create( func, env )
-    ArgAssert( func, 1, "function" )
+do
 
-    local new = {}
-    return new, setfenv( func, LinkTables( new, env or _G ) )
+    local _G = _G
+
+    function Create( func, env )
+        ArgAssert( func, 1, "function" )
+
+        local new = {}
+        return setfenv( func, LinkTables( new, env or _G ) ), new
+    end
+
 end
 
 function SetLinkedTable( env, path, tbl )
-    return table_SetValue( env, path, LinkTables( {}, tbl ) )
+    return table.SetValue( env, path, LinkTables( {}, tbl ) )
 end
 
-function SetTable( env, path, tbl, makeCopy )
-    ArgAssert( env, 1, "table" )
-    ArgAssert( tbl, 3, "table" )
+do
 
-    local object = {}
-    for key, value in pairs( tbl ) do
-        if type( key ) == "table" then
-            key = SetTable( env, nil, tbl, makeCopy )
-        elseif makeCopy and type( key ) == "function" then
-            key = debug_fcopy( setfenv( key, env ) )
+    local pairs = pairs
+
+    function SetTable( env, path, tbl, makeCopy )
+        ArgAssert( env, 1, "table" )
+        ArgAssert( tbl, 3, "table" )
+
+        local object = {}
+        for key, value in pairs( tbl ) do
+            if type( key ) == "table" then
+                key = SetTable( env, nil, tbl, makeCopy )
+            elseif makeCopy and type( key ) == "function" then
+                key = debug_fcopy( setfenv( key, env ) )
+            end
+
+            if type( value ) == "table" then
+                value = SetTable( env, nil, tbl, makeCopy )
+            elseif makeCopy and type( value ) == "function" then
+                value = debug_fcopy( setfenv( value, env ) )
+            end
+
+            object[ key ] = value
         end
 
-        if type( value ) == "table" then
-            value = SetTable( env, nil, tbl, makeCopy )
-        elseif makeCopy and type( value ) == "function" then
-            value = debug_fcopy( setfenv( value, env ) )
+        if path ~= nil then
+            return table.SetValue( env, path, object )
+        else
+            return object
         end
-
-        object[ key ] = value
     end
 
-    if path ~= nil then
-        return table_SetValue( env, path, object )
-    else
-        return object
-    end
 end
