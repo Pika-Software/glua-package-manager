bb85f6ca160740df83e0eb7d6dbc0241484089e9
PrikolMen <olegis019@gmail.com>
2023-04-08 18:42:44 +0400

    Update packages.lua

--- lua/gpm/packages.lua
+++ lua/gpm/packages.lua
@@ -3,11 +3,16 @@ local environment = gpm.environment
 local paths = gpm.paths
 local utils = gpm.utils
 local string = string
+local table = table
 
 -- Variables
 local ErrorNoHaltWithStack = ErrorNoHaltWithStack
 local AddCSLuaFile = AddCSLuaFile
+local ArgAssert = ArgAssert
+local logger = gpm.Logger
 local setfenv = setfenv
+local xpcall = xpcall
+local pairs = pairs
 local type = type
 
 -- Packages table
@@ -33,9 +38,9 @@ end
 function GetMetaData( source )
     if type( source ) == "table" then
         -- Package name, main file & author
-        source.name = isstring( source.name ) and source.name or nil
-        source.main = isstring( source.main ) and source.main or nil
-        source.author = isstring( source.author ) and source.author or nil
+        source.name = type( source.name ) == "string" and source.name or nil
+        source.main = type( source.main ) == "string" and source.main or nil
+        source.author = type( source.author ) == "string" and source.author or nil
 
         -- Version
         local version = source.version
@@ -94,10 +99,7 @@ do
     function PACKAGE:GetIdentifier( name )
         local identifier = string.format( "%s@%s", self:GetName(), utils.Version( self:GetVersion() ) )
         if name then
-            if isstring( name ) then
-                return identifier .. "::" .. name
-            end
-
+            if type( name ) == "string" then return identifier .. "::" .. name end
             return name
         end
 
@@ -132,12 +134,18 @@ do
         return fileList
     end
 
-    function IsPackage( any )
-        return getmetatable( any ) == PACKAGE
-    end
+    do
 
-    list.Set( "GPM - Type Names", TYPE_PACKAGE, "Package" )
-    gpm.SetTypeID( TYPE_PACKAGE, IsPackage )
+        local getmetatable = getmetatable
+
+        function IsPackage( any )
+            return getmetatable( any ) == PACKAGE
+        end
+
+        list.Set( "GPM - Type Names", TYPE_PACKAGE, "Package" )
+        gpm.SetTypeID( TYPE_PACKAGE, IsPackage )
+
+    end
 
 end
 
@@ -169,103 +177,144 @@ function FindFilePath( fileName, files )
     return files[ fileName ] and fileName
 end
 
-local packages = gpm.packages
+do
 
-function Initialize( metadata, func, files, parentPackage )
-    ArgAssert( metadata, 1, "table" )
-    ArgAssert( func, 2, "function" )
-    ArgAssert( files, 3, "table" )
+    local setmetatable = setmetatable
+    local packages = gpm.packages
+    local tostring = tostring
+    local SysTime = SysTime
 
-    local versions = pkgs[ metadata.name ]
-    if versions ~= nil then
-        local gPackage = versions[ metadata.version ]
-        if IsPackage( gPackage ) then
-            if not metadata.isolation and IsPackage( parentPackage ) then
-                environment.LinkMetaTables( parentPackage.environment, gPackage.environment )
+    function Initialize( metadata, func, files, parentPackage )
+        ArgAssert( metadata, 1, "table" )
+        ArgAssert( func, 2, "function" )
+        ArgAssert( files, 3, "table" )
+
+        local versions = pkgs[ metadata.name ]
+        if versions ~= nil then
+            local gPackage = versions[ metadata.version ]
+            if IsPackage( gPackage ) then
+                if not metadata.isolation and IsPackage( parentPackage ) then
+                    environment.LinkMetaTables( parentPackage.environment, gPackage.environment )
+                end
+
+                return gPackage.result
             end
-
-            return gPackage.result
-        end
-    end
-
-    -- Measuring package startup time
-    local stopwatch = SysTime()
-
-    -- Creating package object
-    local gPackage = setmetatable( {}, PACKAGE )
-    gPackage.metadata = metadata
-    gPackage.files = files
-
-    if metadata.logger then
-        gPackage.logger = gpm.logger.Create( gPackage:GetIdentifier(), metadata.color )
-    end
-
-    if metadata.isolation then
-
-        -- Creating environment for package
-        local packageEnv = environment.Create( func )
-        gPackage.environment = packageEnv
-
-        -- Adding to the parent package
-        if IsPackage( parentPackage ) then
-            environment.LinkMetaTables( parentPackage.environment, packageEnv )
         end
 
-        -- Binding package object to gpm.Package
-        environment.SetLinkedTable( packageEnv, "gpm", gpm )
+        -- Measuring package startup time
+        local stopwatch = SysTime()
 
-        -- Globals
-        table.SetValue( packageEnv, "gpm.Logger", gPackage.logger, true )
-        table.SetValue( packageEnv, "gpm.Package", gPackage, true )
-        table.SetValue( packageEnv, "_VERSION", metadata.version )
-        table.SetValue( packageEnv, "promise", gpm.promise )
-        table.SetValue( packageEnv, "TypeID", gpm.TypeID )
-        table.SetValue( packageEnv, "type", gpm.type )
+        -- Creating package object
+        local gPackage = setmetatable( {}, PACKAGE )
+        gPackage.metadata = metadata
+        gPackage.files = files
 
-        environment.SetValue( packageEnv, "import", function( filePath, async, parentPackage )
-            return gpm.Import( filePath, async, parentPackage or gpm.Package )
-        end )
+        if metadata.logger then
+            gPackage.logger = gpm.logger.Create( gPackage:GetIdentifier(), metadata.color )
+        end
 
-        -- include
-        environment.SetValue( packageEnv, "include", function( fileName )
-            local path = packages.FindFilePath( fileName, files )
+        if metadata.isolation then
 
-            if path and files[ path ] then
-                return packages.Run( gpm.Package, files[ path ] )
+            -- Creating environment for package
+            local packageEnv = environment.Create( func )
+            gPackage.environment = packageEnv
+
+            -- Adding to the parent package
+            if IsPackage( parentPackage ) then
+                environment.LinkMetaTables( parentPackage.environment, packageEnv )
             end
 
-            ErrorNoHaltWithStack( "Couldn't include file '" .. tostring( fileName ) .. "' - File not found" )
-        end )
+            -- Binding package object to gpm.Package
+            environment.SetLinkedTable( packageEnv, "gpm", gpm )
 
-        -- AddCSLuaFile
-        if SERVER then
-            environment.SetValue( packageEnv, "AddCSLuaFile", function( fileName )
-                if fileName == nil then fileName = paths.Localize( utils.GetCurrentFile() ) end
-                local path = packages.FindFilePath( fileName, files )
-                if path then return AddCSLuaFile( path ) end
+            -- Globals
+            table.SetValue( packageEnv, "gpm.Logger", gPackage.logger, true )
+            table.SetValue( packageEnv, "gpm.Package", gPackage, true )
+            table.SetValue( packageEnv, "_VERSION", metadata.version )
+            table.SetValue( packageEnv, "promise", gpm.promise )
+            table.SetValue( packageEnv, "TypeID", gpm.TypeID )
+            table.SetValue( packageEnv, "type", gpm.type )
 
-                ErrorNoHaltWithStack( "Couldn't AddCSLuaFile file '" .. tostring( fileName ) .. "' - File not found" )
+            environment.SetValue( packageEnv, "import", function( filePath, async, parentPackage )
+                return gpm.Import( filePath, async, parentPackage or gpm.Package )
             end )
+
+            -- include
+            environment.SetValue( packageEnv, "include", function( fileName )
+                local path = packages.FindFilePath( fileName, files )
+
+                if path and files[ path ] then
+                    return packages.Run( gpm.Package, files[ path ] )
+                end
+
+                ErrorNoHaltWithStack( "Couldn't include file '" .. tostring( fileName ) .. "' - File not found" )
+            end )
+
+            -- AddCSLuaFile
+            if SERVER then
+                environment.SetValue( packageEnv, "AddCSLuaFile", function( fileName )
+                    if fileName == nil then fileName = paths.Localize( utils.GetCurrentFile() ) end
+                    local path = packages.FindFilePath( fileName, files )
+                    if path then return AddCSLuaFile( path ) end
+
+                    ErrorNoHaltWithStack( "Couldn't AddCSLuaFile file '" .. tostring( fileName ) .. "' - File not found" )
+                end )
+            end
+
         end
 
+        -- Run
+        local ok, result = SafeRun( gPackage, func, ErrorNoHaltWithStack )
+        if not ok then
+            logger:Warn( "Package `%s` failed to load, see above for the reason, it took %.4f seconds.", gPackage, SysTime() - stopwatch )
+            return
+        end
+
+        -- Saving result to gPackage
+        gPackage.result = result
+
+        -- Saving in global table & final log
+        logger:Info( "Package `%s` was successfully loaded, it took %.4f seconds.", gPackage, SysTime() - stopwatch )
+
+        local packageName = gPackage:GetName()
+        pkgs[ packageName ] = pkgs[ packageName ] or {}
+        pkgs[ packageName ][ gPackage:GetVersion() ] = gPackage
+
+        return result
     end
 
-    -- Run
-    local ok, result = SafeRun( gPackage, func, ErrorNoHaltWithStack )
-    if not ok then
-        gpm.Logger:Warn( "Package `%s` failed to load, see above for the reason, it took %.4f seconds.", gPackage, SysTime() - stopwatch )
-        return
-    end
+end
 
-    -- Saving result to gPackage
-    gPackage.result = result
+if SERVER then
 
-    -- Saving in global table & final log
-    gpm.Logger:Info( "Package `%s` was successfully loaded, it took %.4f seconds.", gPackage, SysTime() - stopwatch )
+    concommand.Add( "gpm_list", function( ply )
+        if ply ~= nil and not ply:IsListenServerHost() then return end
 
-    local packageName = gPackage:GetName()
-    pkgs[ packageName ] = pkgs[ packageName ] or {}
-    pkgs[ packageName ][ gPackage:GetVersion() ] = gPackage
+        logger:Info( "Package list:" )
+        for name, versions in pairs( pkgs ) do
+            local vTbl = {}
+            for version in pairs( versions ) do
+                vTbl[ #vTbl + 1 ] = utils.Version( version )
+            end
 
-    return result
+            logger:Info( "%s@%s", name, table.concat( vTbl, ", " ) )
+        end
+    end )
+
+end
+
+if CLIENT then
+
+    concommand.Add( "gpm_list", function()
+        logger:Info( "Package list:" )
+        for name, versions in pairs( pkgs ) do
+            local vTbl = {}
+            for version in pairs( versions ) do
+                vTbl[ #vTbl + 1 ] = utils.Version( version )
+            end
+
+            logger:Info( "%s@%s", name, table.concat( vTbl, ", " ) )
+        end
+    end )
+
 end
