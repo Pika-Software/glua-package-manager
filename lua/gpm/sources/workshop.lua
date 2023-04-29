44ba46f0eff9d932d417e782b4c45eb8a4348d41
PrikolMen <olegis019@gmail.com>
2023-04-29 20:38:32 +0400

    Updated workshop source

--- lua/gpm/sources/workshop.lua
+++ lua/gpm/sources/workshop.lua
@@ -1,21 +1,23 @@
+-- https://github.com/WilliamVenner/gmsv_workshop
+if SERVER and not steamworks then
+    if not util.IsBinaryModuleInstalled( "workshop" ) then return end
+    require( "workshop" )
+end
+
 -- Libraries
 local sources = gpm.sources
 local promise = gpm.promise
 local gmad = gpm.gmad
 local fs = gpm.fs
 
--- https://github.com/WilliamVenner/gmsv_workshop
-if SERVER and not steamworks and util.IsBinaryModuleInstalled( "workshop" ) then
-    require( "workshop" )
-end
-
 -- Variables
 local steamworks = steamworks
+local logger = gpm.Logger
 local tonumber = tonumber
 local type = type
 
-local realmFolder = "gpm/" .. ( SERVER and "server" or "client" ) .. "/packages/"
-fs.CreateDir( realmFolder )
+local cacheFolder = "gpm/" .. ( SERVER and "server" or "client" ) .. "/workshop/"
+fs.CreateDir( cacheFolder )
 
 local cacheLifetime = GetConVar( "gpm_cache_lifetime" )
 
@@ -25,44 +27,59 @@ function CanImport( filePath )
     return type( tonumber( filePath ) ) == "number"
 end
 
-Import = promise.Async( function( wsid, parentPackage )
+function Download( wsid )
     local p = promise.New()
 
     steamworks.DownloadUGC( wsid, function( filePath, fileClass )
-        if not fs.Exists( filePath, "GAME" ) then
-            if not fileClass then return p:Reject( "there is no data to read..." ) end
-
-            local gma = gmad.Read( fileClass )
-            if not gma then return p:Reject( "gma reading error" ) end
-
-            local outputPath = realmFolder .. "/workshop_" .. wsid .. ".gma.dat"
-            local fileExists = fs.Exists( outputPath, "DATA" )
-
-            if not fileExists or fs.Time( outputPath, "DATA" ) > ( 60 * 60 * cacheLifetime:GetInt() ) then
-                if fileExists then fs.Delete( outputPath ) end
-
-                local output = gmad.Write( outputPath )
-                if output then
-                    output.Metadata = gma.Metadata
-                    output.Files = gma.Files
-                    output:Close()
-                end
-            end
-
-            if not fs.Exists( outputPath, "DATA" ) then
-                p:Reject( "gma writing error" )
-                return
-            end
-
-            p:Resolve( "data/" .. outputPath )
+        if fs.Exists( filePath, "GAME" ) then
+            p:Resolve( filePath )
             return
         end
 
-        p:Resolve( filePath )
+        if fileClass then
+            local gmaReader = gmad.Read( fileClass )
+            if not gmaReader then
+                p:Reject( "gma reading failed" )
+                return
+            end
+
+            local cachePath = cacheFolder .. wsid .. ".gma.dat"
+            if fs.Exists( cachePath, "DATA" ) then
+                if fs.Time( cachePath, "DATA" ) <= ( 60 * 60 * cacheLifetime:GetInt() ) then
+                    p:Resolve( "data/" .. cachePath )
+                    return
+                end
+
+                fs.Delete( cachePath )
+            end
+
+            gmaReader:ReadAllFiles()
+            gmaReader:Close()
+
+            local gmaWriter = gmad.Write( cachePath )
+            if gmaWriter then
+                gmaWriter.Metadata = gmaReader.Metadata
+                gmaWriter.Files = gmaReader.Files
+                gmaWriter:Close()
+            elseif fs.Exists( cachePath, "DATA" ) then
+                logger:Warn( "Cache writing failed, probably file `data/" .. cachePath .. "` was already mounted, need to restart the game." )
+            else
+                p:Reject( "gma file writing failed." )
+                return
+            end
+
+            p:Resolve( "data/" .. cachePath )
+            return
+        end
+
+        p:Reject( "gma has no data to read." )
     end )
 
-    local ok, result = p:SafeAwait()
-    if not ok then return promise.Reject( result ) end
+    return p
+end
 
+Import = promise.Async( function( wsid, parentPackage )
+    local ok, result = Download( wsid ):SafeAwait()
+    if not ok then return promise.Reject( "Workshop package `" .. wsid .. "` import failed: " .. result ) end
     return sources.gmad.Import( result, parentPackage )
 end )
