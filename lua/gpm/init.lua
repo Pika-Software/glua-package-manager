733db04e3d02cf17cf4182a539ebefaace99f5ce
PrikolMen <olegis019@gmail.com>
2023-04-29 19:50:26 +0400

    Update init.lua
    Co-Authored-By: nullcalv1nism <62751058+CalvinZer0@users.noreply.github.com>

--- lua/gpm/init.lua
+++ lua/gpm/init.lua
@@ -9,7 +9,7 @@ CreateConVar( "gpm_cache_lifetime", "24", FCVAR_ARCHIVE, " - the cache lifetime,
 
 module( "gpm" )
 
-_VERSION = 010900
+_VERSION = 011000
 
 function IncludeComponent( filePath )
     filePath = "gpm/" .. filePath  .. ".lua"
