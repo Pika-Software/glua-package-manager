b3d0f7d35fb743a227edd9f7910bcf5d1a3f37d9
PrikolMen <olegis019@gmail.com>
2023-05-13 06:05:24 +0400

    Zip Source Update

--- lua/gpm/sources/zip.lua
+++ lua/gpm/sources/zip.lua
@@ -1,76 +1,24 @@
 -- Libraries
-local deflatelua = gpm.libs.deflatelua
 local promise = gpm.promise
 local sources = gpm.sources
+local paths = gpm.paths
 local gmad = gpm.gmad
 local string = string
 local table = table
+local zip = gpm.zip
 local util = util
 local fs = gpm.fs
 
 -- Variables
 local cacheLifetime = GetConVar( "gpm_cache_lifetime" )
-local ErrorNoHaltWithStack = ErrorNoHaltWithStack
-local debug_fempty = debug.fempty
+local cacheFolder = gpm.CachePath
 local logger = gpm.Logger
-local tostring = tostring
 local ipairs = ipairs
-local xpcall = xpcall
-local type = gpm.type
-
-local cacheFolder = "gpm/" .. ( SERVER and "server" or "client" ) .. "/packages/"
-fs.CreateDir( cacheFolder )
 
 module( "gpm.sources.zip" )
 
 function CanImport( filePath )
-    return fs.Exists( filePath, "GAME" ) and string.EndsWith( filePath, ".zip.dat" ) or string.EndsWith( filePath, ".zip" )
-end
-
-function IterateZipFiles( fileHandle )
-    if type( fileHandle ) ~= "File" then return debug_fempty end
-
-    return function()
-        if fileHandle:Read( 4 ) ~= "PK\x03\x04" then return end
-        fileHandle:Skip( 4 )
-
-        local compressionMethod = fileHandle:ReadUShort()
-        fileHandle:Skip( 4 )
-
-        local crc = fileHandle:ReadULong()
-        local compressedSize = fileHandle:ReadULong()
-        fileHandle:Skip( 4 )
-
-        local fileNameLen = fileHandle:ReadUShort()
-        local extraLen = fileHandle:ReadUShort()
-        local fileName = fileHandle:Read( fileNameLen )
-        fileHandle:Skip( extraLen )
-
-        local data
-        if compressionMethod == 0 then
-            -- No compression
-            data = fileHandle:Read( compressedSize )
-        elseif compressionMethod == 8 then
-            -- Deflate compression
-            local compressedData = fileHandle:Read( compressedSize )
-
-            local out = {}
-            local ok = xpcall( deflatelua.inflate, ErrorNoHaltWithStack, {
-                ["input"] = compressedData,
-                ["output"] = out
-            } )
-
-            if ok then
-                data = table.concat( out )
-            end
-        end
-
-        if data and tostring( crc ) ~= util.CRC( data ) then
-            data = nil
-        end
-
-        return fileName, data
-    end
+    return fs.IsFile( filePath, "GAME" ) and string.EndsWith( filePath, ".zip.dat" ) or string.EndsWith( filePath, ".zip" )
 end
 
 local contentFolders = {
@@ -86,61 +34,62 @@ local contentFolders = {
     ["lua"] = true
 }
 
-function PerformPath( filePath )
-    local result = {}
-
-    local founded = false
-    for _, folder in ipairs( string.Split( filePath, "/" ) ) do
-        if not founded and contentFolders[ folder ] then
-            founded = true
-        end
-
-        if founded then
-            result[ #result + 1 ] = folder
-        end
-    end
-
-    if #result > 0 then
-        return table.concat( result, "/" )
-    end
+function GetInfo( filePath )
+    local importPath = paths.Fix( filePath )
+    return {
+        ["cachePath"] = cacheFolder .. "zip_" .. util.MD5( importPath ) .. ".gma.dat",
+        ["importPath"] = importPath
+    }
 end
 
-Import = promise.Async( function( filePath )
-    local cachePath = cacheFolder .. "zip_" .. util.MD5( filePath ) .. ".gma.dat"
+Import = promise.Async( function( info )
+    local cachePath = info.cachePath
     if fs.Exists( cachePath, "DATA" ) and fs.Time( cachePath, "DATA" ) > ( 60 * 60 * cacheLifetime:GetInt() ) then
-        return sources.gmad.Import( "data/" .. cachePath )
+        return sources.gmad.Import( sources.gmad.GetInfo( "data/" .. cachePath ) )
     end
 
-    local fileClass = fs.Open( filePath, "rb", "GAME" )
+    local importPath = info.importPath
+    local fileClass = fs.Open( importPath, "rb", "GAME" )
     if not fileClass then
-        logger:Error( "Package `%s` import failed, file cannot be readed.", filePath )
+        logger:Error( "Package `%s` import failed, file cannot be readed.", importPath )
         return
     end
 
     local files = {}
-    for filePath, content in IterateZipFiles( fileClass ) do
+    for filePath, content in zip.IterateZipFiles( fileClass ) do
         if not content then continue end
 
-        filePath = PerformPath( filePath )
-        if not filePath then continue end
+        local result = {}
 
-        files[ #files + 1 ] = { filePath, content }
+        local founded = false
+        for _, folder in ipairs( string.Split( filePath, "/" ) ) do
+            if not founded and contentFolders[ folder ] then
+                founded = true
+            end
+
+            if founded then
+                result[ #result + 1 ] = folder
+            end
+        end
+
+        if #result == 0 then continue end
+        files[ #files + 1 ] = { table.concat( result, "/" ), content }
     end
 
     fileClass:Close()
 
     if #files == 0 then
-        logger:Error( "Package `%s` import failed, no files to mount.", filePath )
+        logger:Error( "Package `%s` import failed, no files to mount.", importPath )
         return
     end
 
     local gma = gmad.Write( cachePath )
     if not gma then
-        logger:Error( "Package `%s` import failed, cache construction error, mounting failed.", filePath )
+        logger:Error( "Package `%s` import failed, cache construction error, mounting failed.", importPath )
         return
     end
 
-    gma:SetTitle( filePath )
+    gma:SetTitle( importPath )
 
     for _, data in ipairs( files ) do
         gma:AddFile( data[ 1 ], data[ 2 ] )
@@ -148,5 +97,5 @@ Import = promise.Async( function( filePath )
 
     gma:Close()
 
-    return sources.gmad.Import( "data/" .. cachePath )
+    return sources.gmad.Import( sources.gmad.GetInfo( "data/" .. cachePath ) )
 end )
