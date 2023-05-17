dc4a8a63f61f62fc9dcdb9217c21767a6fd36eee
PrikolMen <olegis019@gmail.com>
2023-05-17 23:50:16 +0400

    `gpm.fs` updated

--- lua/gpm/fs.lua
+++ lua/gpm/fs.lua
@@ -1,23 +1,23 @@
-if not asyncio then
-    -- https://github.com/Pika-Software/gm_asyncio
-    if util.IsBinaryModuleInstalled( "asyncio" ) then
-        require( "asyncio" )
-    -- https://github.com/WilliamVenner/gm_async_write
-    elseif not file.AsyncWrite and util.IsBinaryModuleInstalled( "async_write" ) then
-        require( "async_write" )
-    end
+-- https://github.com/Pika-Software/gm_asyncio
+if util.IsBinaryModuleInstalled( "asyncio" ) then
+    gpm.Logger:Info( "A third-party file system API 'asyncio' has been initialized." )
+    require( "asyncio" )
+-- https://github.com/WilliamVenner/gm_async_write
+elseif SERVER and util.IsBinaryModuleInstalled( "async_write" ) then
+    gpm.Logger:Info( "A third-party file system API 'async_write' has been initialized." )
+    require( "async_write" )
 end
 
 -- Libraries
 local promise = gpm.promise
 local asyncio = asyncio
 local string = string
+local table = table
 local file = file
 
 -- Variables
 local CompileString = CompileString
 local math_max = math.max
-local SERVER = SERVER
 local ipairs = ipairs
 local pcall = pcall
 local type = type
@@ -25,14 +25,50 @@ local type = type
 module( "gpm.fs" )
 
 Delete = file.Delete
-Exists = file.Exists
 Rename = file.Rename
-IsFile = file.IsFile
-IsDir = file.IsDir
 Open = file.Open
 Find = file.Find
 Size = file.Size
 Time = file.Time
+
+function Exists( filePath, gamePath )
+    if SERVER then return file.Exists( filePath, gamePath ) end
+    if file.Exists( filePath, gamePath ) then return true end
+
+    local files, folders = file.Find( filePath .. "*", gamePath )
+    if not files or not folders then return false end
+    if #files == 0 and #folders == 0 then return false end
+
+    local splits = string.Split( filePath, "/" )
+    local fileName = splits[ #splits ]
+
+    return table.HasIValue( files, fileName ) or table.HasIValue( folders, fileName )
+end
+
+function IsDir( filePath, gamePath )
+    if SERVER then return file.IsDir( filePath, gamePath ) end
+    if file.IsDir( filePath, gamePath ) then return true end
+
+    local _, folders = file.Find( filePath .. "*", gamePath )
+    if folders == nil or #folders == 0 then return false end
+
+    local splits = string.Split( filePath, "/" )
+    return table.HasIValue( folders, splits[ #splits ] )
+end
+
+function IsFile( filePath, gamePath )
+    if SERVER then
+        return file.Exists( filePath, gamePath ) and not file.IsDir( filePath, gamePath )
+    end
+
+    if file.Exists( filePath, gamePath ) and not file.IsDir( filePath, gamePath ) then return true end
+
+    local files, _ = file.Find( filePath .. "*", gamePath )
+    if not files or #files == 0 then return false end
+    local splits = string.Split( filePath, "/" )
+
+    return table.HasIValue( files, splits[ #splits ] )
+end
 
 function Read( filePath, gamePath, lenght )
     local fileClass = file.Open( filePath, "rb", gamePath )
@@ -85,8 +121,7 @@ Compile = promise.Async( function( filePath, gamePath, handleError )
     return result
 end )
 
-if asyncio ~= nil then
-
+if type( asyncio ) == "table" then
     function AsyncRead( filePath, gamePath )
         local p = promise.New()
 
@@ -156,7 +191,24 @@ function AsyncRead( filePath, gamePath )
     return p
 end
 
-if not file.AsyncWrite or not file.AsyncAppend then
+if type( file.AsyncWrite ) == "function" then
+
+    function AsyncWrite( filePath, fileContent )
+        local p = promise.New()
+
+        if file.AsyncWrite( filePath, fileContent, function( filePath, status )
+            if status ~= 0 then return p:Reject( "Error code: " .. status ) end
+            p:Resolve( {
+                ["filePath"] = filePath
+            } )
+        end ) ~= 0 then
+            p:Reject( "Error code: " .. status )
+        end
+
+        return p
+    end
+
+else
 
     function AsyncWrite( filePath, fileContent )
         local p = promise.New()
@@ -174,6 +226,27 @@ if not file.AsyncWrite or not file.AsyncAppend then
         return p
     end
 
+end
+
+if type( file.AsyncAppen ) == "function" then
+
+    function AsyncAppend( filePath, fileContent )
+        local p = promise.New()
+
+        if file.AsyncAppend( filePath, fileContent, function( filePath, status )
+            if status ~= 0 then return p:Reject( "Error code: " .. status ) end
+            p:Resolve( {
+                ["filePath"] = filePath
+            } )
+        end ) ~= 0 then
+            p:Reject( "Error code: " .. status )
+        end
+
+        return p
+    end
+
+else
+
     function AsyncAppend( filePath, fileContent )
         local p = promise.New()
 
@@ -185,37 +258,4 @@ if not file.AsyncWrite or not file.AsyncAppend then
         return p
     end
 
-    return
-end
-
-if not SERVER then return end
-
-function AsyncWrite( filePath, fileContent )
-    local p = promise.New()
-
-    if file.AsyncWrite( filePath, fileContent, function( filePath, status )
-        if status ~= 0 then return p:Reject( "Error code: " .. status ) end
-        p:Resolve( {
-            ["filePath"] = filePath
-        } )
-    end ) ~= 0 then
-        p:Reject( "Error code: " .. status )
-    end
-
-    return p
-end
-
-function AsyncAppend( filePath, fileContent )
-    local p = promise.New()
-
-    if file.AsyncAppend( filePath, fileContent, function( filePath, status )
-        if status ~= 0 then return p:Reject( "Error code: " .. status ) end
-        p:Resolve( {
-            ["filePath"] = filePath
-        } )
-    end ) ~= 0 then
-        p:Reject( "Error code: " .. status )
-    end
-
-    return p
 end
