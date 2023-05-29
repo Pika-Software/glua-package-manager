0196b821fed51ac33eac6fc43301d291ccaafcb3
PrikolMen <olegis019@gmail.com>
2023-05-29 22:42:48 +0400

    Patch1

--- lua/gpm/init.lua
+++ lua/gpm/init.lua
@@ -26,7 +26,7 @@ MsgN( [[
 
 module( "gpm", package.seeall )
 
-_VERSION = 012900
+_VERSION = 012901
 
 if not Colors then
     Realm = "unknown"
@@ -166,9 +166,7 @@ end
 IncludeComponent "import"
 IncludeComponent "commands"
 
+ImportFolder( "packages", nil, true )
+
 Logger:Info( "Time taken to start-up: %.4f sec.", SysTime() - stopwatch )
 hook.Run( "GPM - Initialized" )
-
-util.NextTick( function()
-    ImportFolder( "packages", nil, true )
-end )
