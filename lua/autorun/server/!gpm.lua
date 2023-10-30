2d4f8191e999f2f0ab7c456b5a9f90e21bc05f88
PrikolMen:-b <44779902+PrikolMen@users.noreply.github.com>
2023-10-30 15:42:30 +0400

    compiled version support

--- lua/autorun/server/!gpm.lua
+++ lua/autorun/server/!gpm.lua
@@ -1,3 +1,5 @@
-require( "moonloader" )
-moonloader.PreCacheDir( "gpm" )
+if util.IsBinaryModuleInstalled( "moonloader" ) and pcall( require, "moonloader" ) and moonloader ~= nil then
+    moonloader.PreCacheDir( "gpm" )
+end
+
 include( "gpm/init.lua" )
