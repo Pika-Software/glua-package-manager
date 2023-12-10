a9620d8d09c95cc597441687bb62494c13a27590
Retro <44505837+dankmolot@users.noreply.github.com>
2023-12-10 13:36:31 +0200

    Refactored autorun

---
+++ lua/autorun/!gpm.lua
@@ -0,0 +1,6 @@
+if SERVER and util.IsBinaryModuleInstalled("moonloader") then
+    require("moonloader")
+end
+
+AddCSLuaFile( "gpm/init.lua" )
+include( "gpm/init.lua" )
--- lua/autorun/server/!gpm.lua
+++
@@ -1,5 +0,0 @@
-if util.IsBinaryModuleInstalled( "moonloader" ) and pcall( require, "moonloader" ) and moonloader ~= nil then
-    moonloader.PreCacheDir( "gpm" )
-end
-
-include( "gpm/init.lua" )
