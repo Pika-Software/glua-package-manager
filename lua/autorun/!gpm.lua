b1f26c5eda5c5157c9082b8690b4bbb2c3306f00
unknown-gd <44779902+unknown-gd@users.noreply.github.com>
2024-06-26 03:21:15 +0400

    Filesystem and path library

--- lua/autorun/!gpm.lua
+++ lua/autorun/!gpm.lua
@@ -1,6 +1,9 @@
-if SERVER and util.IsBinaryModuleInstalled "moonloader" then
-    require "moonloader"
+if SERVER then
+    if util.IsBinaryModuleInstalled( "moonloader" ) then
+        require( "moonloader" )
+    end
+
+    AddCSLuaFile( "gpm/init.lua" )
 end
 
-AddCSLuaFile "gpm/init.lua"
-include "gpm/init.lua"
+include( "gpm/init.lua" )
--- lua/packages/package_v2/init.lua
+++ lua/packages/package_v2/init.lua
@@ -10,7 +10,7 @@ local Promise = gpm.Promise
 print("Got from submodule:", submodule)
 Promise.delay(1):await()
 
-print("I am", _PKG)
+print("I am", __package )
 require "package:b"
 
 return "this package is da best"
