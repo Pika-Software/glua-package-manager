9e0be7af09bfdc465e8b847f72182c1c83900037
Retro <44505837+dankmolot@users.noreply.github.com>
2024-07-14 15:08:08 +0300

    Made custom `include`

--- lua/packages/package_v2/init.lua
+++ lua/packages/package_v2/init.lua
@@ -1,21 +1,19 @@
--- local function print( ... )
---     local tbl = {...}
---     local len = #tbl
---     for i = 1, len do
---         tbl[ i ] = tostring( tbl[ i ] )
---     end
---     Logger:Info( table.concat(tbl, "\t" ) )
--- end
+local function print( ... )
+    local tbl = {...}
+    local len = #tbl
+    for i = 1, len do
+        tbl[ i ] = tostring( tbl[ i ] )
+    end
+    Logger:Info( table.concat(tbl, "\t" ) )
+end
 
--- local submodule = require "./submodule.lua"
+local submodule = include "submodule.lua"
 
--- print("Got from submodule:", submodule)
--- Promise.delay(1):await()
+print("Got from submodule:", submodule)
+Promise.delay(1):await()
 
--- print("I am", __package )
--- require "b"
+print("I am", __package )
+require "b"
 
-local data = require "submodule"
-print(data)
 
 return "this package is da best"
